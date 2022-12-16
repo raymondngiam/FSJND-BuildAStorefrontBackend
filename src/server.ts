09d890f390fd3b3f3ff4286902c505f91605d894
raymondngiam <ha.ngiam87@gmail.com>
2022-12-16 10:50:18 +0800

    feat: implement cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+const corsOption = {
+  origin: 'http://somedomain.com',
+  optionSuccessStatus: 200
+};
 
 app.use(express.json());
+app.use(cors(corsOption));
 
 app.get('/', (req: express.Request, res: express.Response): void => {
   res.send('Hello world!');
